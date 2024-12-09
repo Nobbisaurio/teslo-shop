0d7ef696b6e055f6c4265fc9d2b635482ff88e56
Nobbisaurio <davidxdcastro@gmail.com>
2024-12-09 15:55:14 -0500

    404 categoryPage creado

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,21 @@
 
-export default function () {
+
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+
+
+
+export default async function ( { params }: Props ) {
+
+  const { id } = await params;
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page { id }</h1>
     </div>
   );
 }
