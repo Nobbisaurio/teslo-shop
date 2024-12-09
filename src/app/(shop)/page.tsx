98e805bea905bc4aefb6ef5e837f808a2dc6d67d
Nobbisaurio <davidxdcastro@gmail.com>
2024-12-09 13:21:28 -0500

    diseño top menu

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Hola mundo</h1>
-    <h1 className={titleFont.className}>Hola mundo</h1>
+    
     </>
   );
 }
