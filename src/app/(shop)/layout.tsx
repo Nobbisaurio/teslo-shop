40609c597a0bd915a80e046eb75d42b4c53212a2
Nobbisaurio <davidxdcastro@gmail.com>
2024-12-10 23:04:54 -0500

    product grid, product item

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,18 +1,19 @@
-import {TopMenu} from '@/components';
+import { TopMenu } from '@/components';
 
-export default function ShopLayout({
- children
+export default function ShopLayout( {
+  children
 }: {
- children: React.ReactNode;
-}) {
+  children: React.ReactNode;
+} ) {
   return (
     <main className='min-h-screen '>
 
       <TopMenu />
 
 
-
-      {children}
+      <div className='px-0 sm:px-10'>
+        { children }
+      </div>
     </main>
   );
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,20 @@
-import { titleFont } from '@/config/fonts';
-import Image from "next/image";
+import { ProductGrid, Title } from '@/components';
+import { initialData } from '@/seed/seed';
+
+
+import products = initialData.products
+
 
 export default function Home() {
   return (
     <>
-    
+      <Title 
+        title='tienda'
+        subtitle='todos los productos'
+        className='mb=2'
+      />
+
+      <ProductGrid products={products} />
     </>
   );
 }
