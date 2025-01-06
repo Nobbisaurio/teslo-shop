c2140a2476f8a8b9406d7e7288d7b7ab4ddaa73e
Nobbisaurio <davidxdcastro@gmail.com>
2025-01-06 00:02:04 -0500

    Empty cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -2,6 +2,7 @@ import { QuantitySelector, Title } from '@/components';
 import { initialData } from '@/seed/seed';
 import Image from 'next/image';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 
 const productsInCart = [
@@ -11,6 +12,12 @@ const productsInCart = [
 ];
 
 export default function () {
+
+  if(productsInCart.length === 0) {
+    return redirect('/empty');
+  }
+
+
   return (
     <div className='flex justify-center items-center mb-72 px-10 sm:px-0'>
 
