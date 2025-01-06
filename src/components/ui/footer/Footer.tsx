28ae61ae7015b94f125bafccda74350e30728dd6
Nobbisaurio <davidxdcastro@gmail.com>
2025-01-06 00:22:25 -0500

    auth pages

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,5 +1,32 @@
+import { titleFont } from '@/config/fonts';
+import Link from 'next/link';
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='flex w-full justify-center text-xs mb-10'>
+
+      <Link
+        href='/'
+      >
+        <span className={ `${ titleFont.className } antialiased font-bold` }>Teslo</span>
+        <span>| Shop</span>
+        <span>© { new Date().getFullYear() }</span>
+      </Link>
+      <Link
+        href='/'
+        className='mx-3'
+      >
+        Privacidad & Legal
+      </Link>
+      <Link
+        href='/'
+      >
+        Ubicaciones
+      </Link>
+
+
+
+
+    </div>
+  );
+};
